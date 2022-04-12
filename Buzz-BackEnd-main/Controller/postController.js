adea2bc64051c32d1e80c818ce9abdbe150b6f17
KunwarAnasAli <kunwar.ali@tothenew.com>
2022-04-12 15:36:04 +0530

    crud post BackEnd

---
+++ Buzz-BackEnd-main/Controller/postController.js
@@ -0,0 +1,139 @@
+const postModel = require('../userModel/postModel');
+const userModel = require('../userModel/userModel');
+
+// create post
+async function createPost(req, res) {
+  try {
+    const {userId , desc} =  req.body
+    const post = {
+      userId,
+      desc
+    }
+    const createdPost = await postModel.create(post);
+    res.json({
+      message: 'post created',
+      data: createPost
+    })
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
+}
+
+//get post 
+
+async function getPost(req, res){
+  try{
+    let postId = req.params.id;
+    let post = await postModel.findById(postId);
+    if(post){
+      res.json(post)
+    }else{
+      res.status(404).json("post not found")
+    }
+  }catch(err){
+    res.status(500).json(err.message)
+  }
+}
+
+// update post
+
+async function updatePost(req, res) {
+  console.log("update post called");
+  try {
+    const postId = req.params.id
+    const { userId } = req.body;
+    const post = await postModel.findById(postId);
+    if (post) {
+      if (post.userId === userId) {
+        await post.updateOne({ $set: req.body });
+        res.status(200).json("post has been updated")
+      } else {
+        res.status(400).json('Bad request')
+      }
+    } else {
+      res.status(500).json('post not found')
+    }
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
+}
+
+// Delete post
+
+async function deletePost(req, res) {
+  console.log("delete post called");
+  try {
+    const postId = req.params.id;
+    const { userId } = req.body;
+    const post = await postModel.findById(postId);
+    if (post) {
+      if (post.userId === userId) {
+        await post.deleteOne();
+        res.status(200).json("post has been deleted");
+      } else {
+        res.status(400).json('Bad request');
+      }
+    } else {
+      res.status(500).json('post not found');
+    }
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+}
+
+// like post 
+
+async function likePost(req, res) {
+  console.log("like post called");
+  try {
+    const postId = req.params.id;
+    const { userId } = req.body
+    const post = await postModel.findById(postId);
+    if (post) {
+      if (post.likes.includes(userId)) {
+        await post.updateOne({ $pull: { likes: userId } });
+        res.json('post like removed')
+      } else {
+        await post.updateOne({ $push: { likes: userId } });
+        res.json('post liked');
+      }
+    } else {
+      res.status(404).json('post not found')
+    }
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
+}
+
+// feeds post
+
+async function feedsPost(req , res){
+  console.log("feedsPost");
+ try{
+  let {userId} = req.body;
+  let user = await userModel.findById(userId);
+  if(user){
+    const userPosts = await postModel.find({userId:user._id});
+    const friends = await user.friends;
+    //console.log(friends);
+    let friendsPost = await Promise.all(friends.map((id)=>{
+      return postModel.find({userId:id});
+    }))
+    const allPost = userPosts.concat(...friendsPost);
+    res.json(allPost);
+  }else{
+    res.json('no user found')
+  }
+ }catch(err){
+   res.status(500).json(err.message)
+ }
+}
+
+module.exports = {
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+  getPost,
+  feedsPost
+}
